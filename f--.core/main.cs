3138e12053100d4c4c07fc2e7f25f7326d2a243c
randomusert <127417963+randomusert@users.noreply.github.com>
2023-07-02 18:17:47 +0300

    file setup done in visual studio

---
+++ f--.core/main.cs
@@ -0,0 +1,22 @@
+﻿//usings
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//https://tinyurl.com/448548 for github repo
+
+namespace f__.core
+{
+    internal class main
+    {
+    }
+}
+/*usings
+ * using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+ */
